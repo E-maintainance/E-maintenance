1bb741d0b6692a98fe6b3357d62bb772a4e64156
naeemalomari <naeemalomari96@gmail.com>
2021-06-13 17:16:44 +0300

    changes

--- js/rent.js
+++ js/rent.js
@@ -1,14 +1,30 @@
 
-let z = document.getElementById('but');
-z.addEventListener('click',handelLink);
+// let z = document.getElementById('but');
+// z.addEventListener('click',handelLink);
 
 
 
-function handelLink(){
-    let a = document.getElementById('choice');
-    // let selectedV;
-    if(document.getElementsByName('true').checked == true){
-        alert('woow');
-    }
-}
-handelLink();
+// function handelLink(event){
+//   event.preventdefault ();
+//   let a = document.getElementById('choice');
+//   // let selectedV;
+//   if(document.getElementsByName('true').checked == true){
+//     alert('WOOW');
+//   }
+// }
+// handelLink();
+
+document.getElementById('but').addEventListener('click',function (){
+//   alert ('hello!!');
+  if (document.getElementById('yes').checked == true )
+  {
+    location.replace('../pages/rent.html');
+  }
+});
+
+// document.getElementById('but').addEventListener('click',myFunction());
+// function myFunction() {
+
+//   location.replace('http://127.0.0.1:5501/pages/rent.html');
+// }
+// myFunction();

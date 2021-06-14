1f80ec444604affeae7a051a7c0c9b58910322c5
naeemalomari <naeemalomari96@gmail.com>
2021-06-14 12:12:56 +0300

    adding darkmode

---
+++ js/all.js
@@ -0,0 +1,6 @@
+let content = document.getElementsByTagName('body');
+let darkMode =document.getElementsByI('dark-change');
+darkMode.addEventListener('click',function(){
+  darkMode.classlist.toggle('checkbox');
+  content.classList.toggle('check');
+});

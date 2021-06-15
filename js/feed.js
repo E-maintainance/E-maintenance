48d17f4af7026b394694379494280da0f71a5643
zaidsh98 <sha7rouri29@gmail.com>
2021-06-15 09:06:28 +0300

    zzaa

---
+++ js/feed.js
@@ -0,0 +1,75 @@
+'use strict';
+
+let form = document.getElementById('user');
+let feedbackArr = [];
+
+
+function Feedback(name, feedback) {
+  this.name = name;
+  this.feedback = feedback;
+  feedbackArr.push(this);
+}
+
+if (localStorage.getItem('feedback')) {
+  gettingData();
+}
+
+
+
+
+function render() {
+
+
+
+  let ulEl = document.getElementById('ul');
+
+
+  for (let i = 0; i < feedbackArr.length; i++) {
+    let list = document.createElement('li');
+    list.setAttribute('id', 'li');
+    ulEl.appendChild(list);
+
+    let h2El = document.createElement('h2');
+    list.appendChild(h2El);
+    h2El.id = 'h2';
+    h2El.textContent = feedbackArr[i].name;
+
+    let pEl = document.createElement('p');
+    pEl.id = 'textP';
+    list.appendChild(pEl);
+    pEl.textContent = feedbackArr[i].feedback;
+  }
+
+  
+}
+
+render();
+
+
+
+
+
+
+form.addEventListener('submit', handleFeedback);
+
+function handleFeedback(event) {
+  event.preventDefault();
+  let zaid = event.target.name.value;
+  let asia = event.target.feedback.value;
+  new Feedback(zaid, asia);
+  saving();
+  render();
+  location.reload();
+}
+
+function saving() {
+  let userNameLs = JSON.stringify(feedbackArr);
+  localStorage.setItem('feedback', userNameLs);
+}
+
+function gettingData() {
+  let getFeed = localStorage.getItem('feedback');
+  feedbackArr = JSON.parse(getFeed);
+}
+
+
--- js/rent2.js
+++ js/rent2.js
@@ -1,69 +1,68 @@
-let rent = document.getElementsByClassName("tag")
+let rent = document.getElementsByClassName('tag');
 let i;
 let x;
-console.log(rent)
+console.log(rent);
 // let rent2 =document.getElementsByClassName()
 // let rent3 =document.getElementsByClassName()
 for( i=0;i<rent.length;i++){
-rent[i].addEventListener("click",handlclick);
+  rent[i].addEventListener('click',handlclick);
 
 }
-console.log(i)
+console.log(i);
 function handlclick(event){
-    console.log(event.target.innerHTML)
-    
-    event.preventDefault();
-    if(event.target.innerHTML=="Kia Rio 3"){
-        x=14.99;
-        
-        new pay("Kia Rio 3",x);
-        
-    }
-    else if(event.target.innerHTML=="Renault Clio"){
-        x=19.99
-        
-        new pay("Renault Clio",x)
+  console.log(event.target.innerHTML);
 
-    }
-    else if(event.target.innerHTML=="Honda Civic 1999"){
-        x=9.99;
-        
-        new pay("Honda Civic 1999",x);
-    }
-    saving();
-    swal.fire({
-        title: `Are you sure you want ${event.target.innerHTML} ?`,
-        text: 'Isn`t it the Coolest car ever?',
-        icon: 'question',
-    }).then(function() {
-        location.replace("../pages/payment.html");
-    });
-    }
-    
-   
-    pay.all=[];
-    
-    function pay(car,price){
-        this.car=car;
-        this.price=price;
-        console.log(this.price)
-        pay.all.push(this);
-    
-    }
+  event.preventDefault();
+  if(event.target.innerHTML=='Kia Rio 3'){
+    x=14.99;
 
-    
+    new pay('Kia Rio 3',x);
+
+  }
+  else if(event.target.innerHTML=='Renault Clio'){
+    x=19.99;
+
+    new pay('Renault Clio',x);
+
+  }
+  else if(event.target.innerHTML=='Honda Civic 1999'){
+    x=9.99;
+
+    new pay('Honda Civic 1999',x);
+  }
+  saving();
+  swal.fire({
+    title: `Are you sure you want ${event.target.innerHTML} ?`,
+    text: 'Isn`t it the Coolest car ever?',
+    icon: 'question',
+  }).then(function() {
+    location.replace('../pages/payment.html');
+  });
+}
+
+
+pay.all=[];
+
+function pay(car,price){
+  this.car=car;
+  this.price=price;
+  console.log(this.price);
+  pay.all.push(this);
+
+}
 
 
 
 
 
-    function saving(){
-        convert=JSON.stringify(pay.all)
-        localStorage.setItem("rent_info",convert);
-        
-        } 
-
-    
 
 
-    
+function saving(){
+  convert=JSON.stringify(pay.all);
+  localStorage.setItem('rent_info',convert);
+
+}
+
+
+
+

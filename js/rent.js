8f5939cbd6401f4b3b6fc75ea9c0643d56f4e882
Kafaween <abod.kafaween@gmail.com>
2021-06-15 09:38:04 +0300

    kk

--- js/rent.js
+++ js/rent.js
@@ -1,8 +1,8 @@
 
-document.getElementById('but').addEventListener('click',function (){
-  //   alert ('hello!!');
-  if (document.getElementById('rent').checked == true )
-  {
-    location.replace('../pages/rent.html');
-  }
-});
+// document.getElementById('but').addEventListener('click',function (){
+//   //   alert ('hello!!');
+//   if (document.getElementById('rent').checked == true )
+//   {
+//     location.replace('../pages/rent.html');
+//   }
+// });
